3ca715935545f4bb34f13006b42e22ebb6a83f4f
Rachel Wyatt <rw126@evansville.edu>
2019-02-17 20:10:25 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(require(:first_name, :last_name))
+    @student = Student.new((student_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
